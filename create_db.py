4aa9b421b18945b122d516fafea1f437b86ba541
dev4ox <dev4ox@gmail.com>
2024-04-02 10:56:24 +0300

    init

---
+++ create_db.py
@@ -0,0 +1,27 @@
+import sqlite3
+db_name = 'db.sqlite'
+
+
+def create():
+    conn = sqlite3.connect(db_name)
+    try:
+        cur = conn.cursor()
+        cur.execute('''CREATE TABLE IF NOT EXISTS quiz (
+                        id INTEGER PRIMARY KEY,
+                        fac INTEGER,
+                        question TEXT,
+                        answer TEXT,
+                        status INTEGER
+                        )''')
+        conn.commit()
+        return True
+    except Exception as e:
+        print('Не удалось создать таблицу', e)
+        return False
+    finally:
+        conn.close()
+
+
+if create():
+    print(f'Таблица "{db_name}" успешно создана!')
+
---
+++ main.py
@@ -0,0 +1,5 @@
+import telebot
+import key
+import menu
+import func
+
